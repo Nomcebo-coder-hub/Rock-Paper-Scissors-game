334cee8587304f3b0d24d76b06cf2c6d139dde9d
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2024-01-09 16:34:31 +0200

    Add event listeners for div classes

--- src/index.js
+++ src/index.js
@@ -7,3 +7,15 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+
+rock_div.addEventListener("click", function () {
+  console.log("hey you clicked on rock");
+});
+
+paper_div.addEventListener("click", function () {
+  console.log("hey you clicked on paper");
+});
+
+scissors_div_div.addEventListener("click", function () {
+  console.log("hey you clicked on scissors");
+});
